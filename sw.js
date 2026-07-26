2468243c64ef9b12b1ef7e10bfcd485749477a29
jeldjor <6nbzd8pykj@privaterelay.appleid.com>
2026-07-26 22:32:12 +0200

    Bingo Beats V204: sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'bingo-beats-clean-v203';
+const CACHE = 'bingo-beats-clean-v204';
 const CORE = [
   './',
   './index.html',
-  './style.css?v=2030',
-  './app.js?v=2030',
+  './style.css?v=2040',
+  './app.js?v=2040',
   './manifest.json',
   './bb_logo.png',
   './bb_logo_lime.webp',
